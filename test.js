6c44beaa03a1f7028eaf966621a3c4da4092f68c
lttsz123 <libaifst1@gmail.com>
2024-07-19 08:43:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const axiosRetry = require('axios-retry').default;
 const fs = require('fs');
 const path = require('path');
-
+const successPath = "success.txt"
 // Faucet API endpoint
-const FAUCET_URL = 'https://faucet.edgenet.allora.network/send/edgenet/';
+const FAUCET_URL = 'https://faucet.testnet-1.testnet.allora.network/send/allora-testnet-1/';
 
 // Confirm axios-retry is loaded
 console.log(`axiosRetry loaded: ${typeof axiosRetry === 'function'}`);
@@ -25,10 +25,15 @@ axios.defaults.timeout = 20000; // 20 seconds
 // Function to claim faucet tokens
 async function claimFaucet(address) {
     try {
-        const response = await axios.get(`${FAUCET_URL}${address}`);
-        if (response.status === 200 && response.data.result.code !== 0) {
-            console.log(`Successfully claimed faucet for address: ${address}`);
-        } else {
+        const response = await axios.get(`${FAUCET_URL}${address}`,);
+        if (response.status === 200 && response.data.result.code === 0) {
+            console.log(`Successfully claimed faucet for address: ${address}.Response: ${JSON.stringify(response.data)}`);
+            fs.appendFileSync(successPath, address+"\n", 'utf8');
+            return true
+        }else if (response.status === 200 && response.data.result.code === 429) {
+            console.log(`error claimed faucet for address: ${address}.Response: ${JSON.stringify(response.data)}`);
+        }
+        else {
             console.error(`Failed to claim faucet for address: ${address}. Response: ${JSON.stringify(response.data)}`);
         }
     } catch (error) {
@@ -40,16 +45,31 @@ async function claimFaucet(address) {
             console.error(`Error claiming faucet for address: ${address}`, error.message);
         }
     }
+    return false
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // Function to read addresses from file and claim faucet
 async function processAddresses(file) {
     try {
         const addresses = fs.readFileSync(file, { encoding: 'utf-8' }).split('\n').map(a => a.trim()).filter(Boolean);
+        const successAddresses = fs.readFileSync(successPath, { encoding: 'utf-8' }).split('\n').map(a => a.trim()).filter(Boolean);
 
         for (const address of addresses) {
+            if(successAddresses.indexOf(address)!==-1){
+                console.log(`这个地址已经成功领水，不用跑了:${address}`)
+            }
             if (address) { // Ensure address is not empty
-                await claimFaucet(address);
+                // let success = false
+                // let many = false;
+                // while (!success){
+                //     success = await claimFaucet(address);
+                    await claimFaucet(address);
+                    await  sleep(1000)
+                // }
             }
         }
     } catch (error) {
@@ -57,15 +77,15 @@ async function processAddresses(file) {
     }
 }
 
-// Get the file name from command line arguments
+// // Get the file name from command line arguments
 const fileName = process.argv[2];
 if (!fileName) {
     console.log('Please provide a file name.');
     process.exit(1);
 }
+//
+// // Ensure the file path is correct
+// const filePath = path.resolve(__dirname, fileName);
 
-// Ensure the file path is correct
-const filePath = path.resolve(__dirname, fileName);
-
-processAddresses(filePath);
+processAddresses(fileName);
 

b808244789b2abf6608d9df90d5dabff51615672
lttsz123 <libaifst1@gmail.com>
2024-07-19 09:46:37 +0800

    Update test.js

--- test.js
+++ test.js
@@ -9,6 +9,26 @@ const FAUCET_URL = 'https://faucet.testnet-1.testnet.allora.network/send/allora-
 // Confirm axios-retry is loaded
 console.log(`axiosRetry loaded: ${typeof axiosRetry === 'function'}`);
 
+// 定义一个包含不同User-Agent的数组
+const userAgents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (Linux; Android 11; Pixel 5 Build/RP1A.201005.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 10; SM-G986U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.54 Mobile Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.3 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (iPad; CPU OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
+];
+
+// 定义一个函数，用于随机选择一个User-Agent
+function getRandomUserAgent() {
+    return userAgents[Math.floor(Math.random() * userAgents.length)];
+}
+
+
 // Configure axios to retry failed requests using axios-retry
 axiosRetry(axios, {
     retries: 3,
@@ -25,7 +45,11 @@ axios.defaults.timeout = 20000; // 20 seconds
 // Function to claim faucet tokens
 async function claimFaucet(address) {
     try {
-        const response = await axios.get(`${FAUCET_URL}${address}`,);
+        const response = await axios.get(`${FAUCET_URL}${address}`, {
+            headers: {
+                'User-Agent': getRandomUserAgent()
+            }
+        });
         if (response.status === 200 && response.data.result.code === 0) {
             console.log(`Successfully claimed faucet for address: ${address}.Response: ${JSON.stringify(response.data)}`);
             fs.appendFileSync(successPath, address+"\n", 'utf8');
@@ -91,4 +115,5 @@ if (!fileName) {
 // const filePath = path.resolve(__dirname, fileName);
 
 processAddresses(fileName);
+
 
